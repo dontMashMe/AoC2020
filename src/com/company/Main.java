424e4d88fb180537ea42656158663aa15930d25d
dontMashMe <marijan.bebek@mail.com>
2021-02-13 00:56:41 +0100

    Day One

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DayOne dayOne = new DayOne();
+        System.out.println(dayOne.Solution());
+    }
+}
